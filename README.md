2261d8c14a96199522b97044246bb42c03bd99ae
Tom Gillard <tomgillar@gmail.com>
2017-07-09 20:52:40 +1000

    Initial commit of required packages, placeholder test and basic project readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Modern JavaScript test starter
+
+A small collection of packages to quickly and easily unit js files using ES6 & Tape
+
+## Dependencies
+
+- Node >= v6.3.1
+- Yarn >= v0.23.0
---
+++ src/demo-test.spec.js
@@ -0,0 +1,12 @@
+import test from 'tape';
+
+// For each unit test you write,
+// answer these questions:
+test('What component aspect are you testing?', assert => {
+  const actual = 'What is the actual output?';
+  const expected = 'What is the expected output?';
+  const description = 'What should the feature do?';
+
+  assert.equal(actual, expected, description);
+  assert.end();
+});
